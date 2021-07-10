e90c3705254b0d95e8b234c1cacf04cb21fa7f36
Vitus543 <falco.abade@gmail.com>
2021-07-10 16:52:03 +0100

    add limits and warps positison x and y

---
+++ Galaxy Shooter/Assets/Scripts/GeneralSettings.cs
@@ -0,0 +1,16 @@
+
+public class GeneralSettings
+{
+    public const string HorizontalInput = "Horizontal";
+
+    public const string VerticalInput = "Vertical";
+
+}
+
+public class PositionsConst
+{
+
+    public const float limitPositionY = -4.2f;
+
+    public const float WrapsPositionX = 9.4f;
+}
--- Galaxy Shooter/Assets/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -1,10 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     public float speed = 5.0f;
+    private float negativePositionX = -PositionsConst.WrapsPositionX;
 
     // Start is called before the first frame update
     void Start()
@@ -15,8 +14,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
+        float horizontalInput = Input.GetAxis(GeneralSettings.HorizontalInput);
+        float verticalInput = Input.GetAxis(GeneralSettings.VerticalInput);
 
+        transformTranslate(horizontalInput, Vector3.right);
+        transformTranslate(verticalInput, Vector3.up);
+
+        CheckPostion();
+    }
+
+    private void transformTranslate(float Input, Vector3 vector3)
+    {
+        transform.Translate(vector3 * speed * Input * Time.deltaTime);
+    }
+
+    private void CheckPostion()
+    {
+        // limits position Y
+        if (transform.position.y > 0)
+        {
+            transform.position = new Vector3(transform.position.x, 0, 0);
+        }
+        else if (transform.position.y < PositionsConst.limitPositionY)
+        {
+            transform.position = new Vector3(transform.position.x, PositionsConst.limitPositionY, 0);
+        }
+
+        //Position wraps X left to right and vice versa
+        if (transform.position.x > PositionsConst.WrapsPositionX)
+        {
+            transform.position = new Vector3(negativePositionX, transform.position.y, 0);
+        }
+        else if (transform.position.x < negativePositionX)
+        {
+            transform.position = new Vector3(PositionsConst.WrapsPositionX, transform.position.y, 0);
+        }
     }
 }
