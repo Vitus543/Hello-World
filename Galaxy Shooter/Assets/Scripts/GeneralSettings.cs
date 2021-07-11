14f2c24a25bff2e7404cb2986ae96cf0eea2051c
Vitus543 <falco.abade@gmail.com>
2021-07-11 19:33:09 +0100

    implement shooting

--- Galaxy Shooter/Assets/Scripts/GeneralSettings.cs
+++ Galaxy Shooter/Assets/Scripts/GeneralSettings.cs
@@ -1,15 +1,20 @@
 
-public class GeneralSettings
+public class ShipMovementInputsConst
 {
     public const string HorizontalInput = "Horizontal";
 
     public const string VerticalInput = "Vertical";
 
 }
-public class PositionsConst
+public class ShipPositionsConst
 {
 
     public const float limitPositionY = -4.2f;
 
-    public const float WrapsPositionX = 9.4f;
+    public const float WrapsPositionX = 8.65f;
+}
+
+public class LaserPositionConst
+{
+    public const float limitPositionY = 6.0f;
 }
--- Galaxy Shooter/Assets/Scripts/Player.cs
+++ Galaxy Shooter/Assets/Scripts/Player.cs
@@ -2,10 +2,20 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject laserPrefab;
+
+    [SerializeField]
+    private float fireRate = 0.25f;
+
+    private float nextFire = 0.0f;
+
     [SerializeField]
     private float speed = 5.0f;
+
     [SerializeField]
-    private readonly float negativePositionX = -PositionsConst.WrapsPositionX;
+    private readonly float negativePositionX = -ShipPositionsConst.WrapsPositionX;
 
     // Start is called before the first frame update
     void Start()
@@ -17,13 +27,15 @@ public class Player : MonoBehaviour
     void Update()
     {
         Movement();
+
+        Shooting();
     }
 
     #region Movement
     private void Movement()
     {
-        float horizontalInput = Input.GetAxis(GeneralSettings.HorizontalInput);
-        float verticalInput = Input.GetAxis(GeneralSettings.VerticalInput);
+        float horizontalInput = Input.GetAxis(ShipMovementInputsConst.HorizontalInput);
+        float verticalInput = Input.GetAxis(ShipMovementInputsConst.VerticalInput);
 
         TransformTranslate(horizontalInput, Vector3.right);
         TransformTranslate(verticalInput, Vector3.up);
@@ -43,19 +55,32 @@ public class Player : MonoBehaviour
         {
             transform.position = new Vector3(transform.position.x, 0, 0);
         }
-        else if (transform.position.y < PositionsConst.limitPositionY)
+        else if (transform.position.y < ShipPositionsConst.limitPositionY)
         {
-            transform.position = new Vector3(transform.position.x, PositionsConst.limitPositionY, 0);
+            transform.position = new Vector3(transform.position.x, ShipPositionsConst.limitPositionY, 0);
         }
 
         //Position wraps X left to right and vice versa
-        if (transform.position.x > PositionsConst.WrapsPositionX)
+        if (transform.position.x > ShipPositionsConst.WrapsPositionX)
         {
             transform.position = new Vector3(negativePositionX, transform.position.y, 0);
         }
         else if (transform.position.x < negativePositionX)
         {
-            transform.position = new Vector3(PositionsConst.WrapsPositionX, transform.position.y, 0);
+            transform.position = new Vector3(ShipPositionsConst.WrapsPositionX, transform.position.y, 0);
+        }
+    }
+    #endregion
+
+    #region Shooting
+    private void Shooting()
+    {
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Vector3 position = transform.position + new Vector3(0, 0.88f, 0);
+            Instantiate(laserPrefab, position, Quaternion.identity);
+            //// Instantiate(projectile, transform.position, transform.rotation);
         }
     }
     #endregion
